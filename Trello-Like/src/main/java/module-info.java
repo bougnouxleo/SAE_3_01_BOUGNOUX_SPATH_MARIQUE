396e0e2c66bc7e87c3233c73e0fa311bd6a3e57a
Leo Bougnoux <e44214u@etu.univ-lorraine.fr>
2025-12-09 17:14:34 +0100

    Delete Trello-Like directory

--- Trello-Like/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.trellolike.trellolike {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.trellolike.trellolike to javafx.fxml;
-    exports org.trellolike.trellolike;
-}
